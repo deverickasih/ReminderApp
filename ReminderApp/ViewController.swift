6256f582a7776c1621c4071868df2d6d1a77a0e1
deverickasih <erick.kasih@gmail.com>
2021-03-06 14:12:00 +0700

    Initial Commit

---
+++ ReminderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReminderApp
+//
+//  Created by JAN FREDRICK on 06/03/21.
+//  Copyright © 2021 JFSK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
